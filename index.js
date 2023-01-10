93b39f2066328cb022b4f4a0b3f8ccfd62fafffe
mike giroux <michaelgiroux909@gmail.com>
2023-01-10 15:09:11 -0800

    commit tree b97d3c751b9748ff1348726d18ce8230ab60a24f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num = num-1;
+    }
+}
+function writeCards(names, string){
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
